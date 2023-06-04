63fea52aca6c08beb24c89fe6a8f66402c7959cf
joaokxt <joaokx.terck@gmail.com>
2023-06-03 21:21:06 -0300

    Game over, vidas, texto

--- project/script.js
+++ project/script.js
@@ -1,19 +1,43 @@
+let enviarFormulario = () =>{
+    
+}
+
 let juego = () => {
     const canvas = document.getElementById("screen");
     const ctx = canvas.getContext("2d");
 
-    var vidas;
-    var puntos
+    var vidas=3;
+    var puntos=0;
+    var nivel=1;
 
     var xPaleta=canvas.width/2;
-    var yPaleta=canvas.height-(canvas.height/8)
+    var yPaleta=canvas.height-(canvas.height/8);
+    var anchoPaleta=50;
+    var alturaPaleta=2;
     var flechaDerecha=false;
     var flechaIzquierda=false;
     
     var xPelota=xPaleta;
     var yPelota=yPaleta-4;
+    var radioPelota=2;
     var dx=2;
     var dy=-2;
+
+    var filasLadrillos = 3;
+    var columnasLadrillos = 5;
+    var anchoLadrillo = 75;
+    var alturaLadrillo = 20;
+    var paddingLadrillo = 10;
+    var margenSuperior = 30;
+    var margenIzquierdo = 30;
+    var ladrillos = [];
+    for(c=0; c<columnasLadrillos; c++) {
+        ladrillos[c] = [];
+    }
+    for(r=0; r<filasLadrillos; r++) {
+        ladrillos[c][r] = { x: 0, y: 0 };
+    }
+
 
     document.addEventListener(
         "keydown", 
@@ -41,7 +65,7 @@ let juego = () => {
 
     let dibujarPelota = () => {
         ctx.beginPath();
-        ctx.arc(xPelota, yPelota, 2, 0, Math.PI*2);
+        ctx.arc(xPelota, yPelota, radioPelota, 0, Math.PI*2);
         ctx.fillStyle="#ffffff";
         ctx.fill();
         ctx.closePath();
@@ -49,32 +73,96 @@ let juego = () => {
 
     let dibujarPaleta = () => {
         ctx.beginPath();
-        ctx.rect(xPaleta-25, yPaleta, 50, 2);
+        ctx.rect(xPaleta-25, yPaleta, anchoPaleta, alturaPaleta);
         ctx.fillStyle="#ffffff";
         ctx.fill();
         ctx.closePath();
     }
-    
-    let dibujar = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        dibujarPelota();
-        dibujarPaleta();
-        if(xPelota+dx>canvas.width || xPelota+dx<0){
-            dx*=-1;
-        }
-        if(yPelota+dy>canvas.height || yPelota+dy<0){
-            dy*=-1;
-        }
-        if(flechaDerecha==true && xPaleta<canvas.width-25){
-            xPaleta+=2;
-        }
-        if(flechaIzquierda==true && xPaleta>25){
-            xPaleta-=2;
-        }
-        xPelota+=dx;
-        yPelota+=dy;
+
+    let dibujarLadrillos = () => {
+
     }
 
-    setInterval(dibujar, 10);
+    let gameOver = () => {
+        ctx.font = "20px Arial";
+        ctx.fillText("GAME OVER", canvas.width/2-60, canvas.height/2);
+        mostrarBoton();
+    }
+    
+    let dibujar = () => {
+        //LIMPIAR CANVAS
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //DIBUJAR ELEMENTOS
+        dibujarPelota();
+        dibujarPaleta();
+
+        //CALCULO REBOTE PELOTA CON PARED
+        if(xPelota+dx>canvas.width-radioPelota || xPelota+dx<radioPelota){
+            dx*=-1;
+        }
+        if(yPelota+dy<radioPelota){
+            dy*=-1;
+        }else if(yPelota+dy>canvas.height-radioPelota){
+            vidas=vidas-1;
+            if(vidas===0){
+                gameOver();
+                clearInterval(intervalo);
+            }else{
+                xPaleta=canvas.width/2;
+                yPaleta=canvas.height-(canvas.height/8);
+                xPelota=xPaleta;
+                yPelota=yPaleta-4;
+                dx=2;
+                dy=-2;
+            }
+        }
+        //CALCULO REBOTE PELOTA CON PALETA
+        if(yPelota+dy==yPaleta && (xPelota+dx>xPaleta-anchoPaleta/2 && xPelota+dx<xPaleta+anchoPaleta/2)){
+            dy*=-1;
+        }
+
+        //MOVIMIENTO PELOTA
+        xPelota+=dx;
+        yPelota+=dy;
+
+        
+        //MOVIMIENTO PALETA
+        if(flechaDerecha==true && xPaleta<canvas.width-anchoPaleta/2){
+            xPaleta+=2;
+        }
+        if(flechaIzquierda==true && xPaleta>anchoPaleta/2){
+            xPaleta-=2;
+        }
+
+        //MOSTRAR STATUS
+        if(puntos>=100){
+            document.getElementById("puntos").textContent=puntos;
+        }else if(puntos>=10){
+            document.getElementById("puntos").textContent="0"+puntos;
+        }else{
+            document.getElementById("puntos").textContent="00"+puntos;
+        }
+        document.getElementById("vidas").textContent="00"+vidas;
+        if(nivel>=100){
+            document.getElementById("nivel").textContent=nivel;
+        }else if(nivel>=10){
+            document.getElementById("nivel").textContent="0"+nivel;
+        }else{
+            document.getElementById("nivel").textContent="00"+nivel;
+        }
+    }
+
+    let intervalo=setInterval(dibujar, 10);
+    
 }
+
+let mostrarBoton = () => {
+    document.getElementById("iniciar").style.display = 'block';
+}
+
+let ocultarBoton = () => {
+    document.getElementById("iniciar").style.display = 'none';
+}
+
 
